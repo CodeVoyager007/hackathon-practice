a0d6861ad922889e27abde1c33cd2d9569f7696d
Ayesha Mughal <ayeshamughal2162@gmail.com>
2025-11-25 11:00:08 +0500

    fix: broken link on main page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/">
+            to="/introduction-to-ai">
             Start Reading "A Beginner's Guide to AI" 🚀
           </Link>
         </div>
